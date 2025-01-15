901716c0908155414e5a57499804e43f41556d68
Charan <yourscharangoud@gmail.com>
2025-01-16 01:07:24 +0530

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-background flex flex-col justify-center items-center text-center h-screen w-screen">
+      <h1 className="text-white text-4xl">RGlitz Technologies</h1>
+      <p className="bg-white text-background rounded-xl px-2 mt-1">A Creative Digital Marketing Agency in Hyderabad</p>
+    </div>
+  );
+}
