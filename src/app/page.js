499a93e775613228818018d7436c734877d171f2
Charan <yourscharangoud@gmail.com>
2025-01-16 01:21:35 +0530

    auto deploy check

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
     <div className="bg-background flex flex-col justify-center items-center text-center h-screen w-screen">
       <h1 className="text-white text-4xl">RGlitz Technologies</h1>
       <p className="bg-white text-background rounded-xl px-2 mt-1">A Creative Digital Marketing Agency in Hyderabad</p>
+      <p>+91 76708 32977</p>
     </div>
   );
 }
