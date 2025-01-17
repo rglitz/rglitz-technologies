7e18008bc0d27d5287e27c168b2ecfc24076ce44
sai kumar <saik62108@gmail.com>
2025-01-17 17:07:02 +0530

    kumar commit

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ export default function Home() {
     <div className="flex flex-col justify-center items-center text-center h-screen">
       <h1 className=" text-4xl">RGlitz Technologies</h1>
       <p className=" rounded-xl px-2 mt-1">A Creative Digital Marketing Agency in Hyderabad</p>
+      <p className=" rounded-xl px-2 mt-1">updated by sai kumar</p>
       <p>info@rglitz.com | +91 76708 32977</p>
     </div>
   );
